e9dacd7ec3efac5a69e7a37d86bcb9bfc3c6ed39
Rabbeg mouhib <rabbegmouhib@gmail.com>
2020-04-26 18:17:52 +0200

    updated for deployment

--- utilits/email.js
+++ utilits/email.js
@@ -23,27 +23,6 @@ module.exports = class Email {
           pass: 'EZfBJRg3ufbjTPAruv',
         },
       });
-    } else {
-      return nodemailer.createTransport({
-        /* //email trap to simulate email sending
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD,
-        }, */
-
-        port: process.env.PORT,
-        host: 'https://serene-escarpment-16292.herokuapp.com/',
-        service: 'Hotmail',
-        auth: {
-          user: `${process.env.EMAIL_FROM}`,
-          pass: 'Taekwondo',
-        },
-        tls: {
-          rejectUnauthorized: false,
-        },
-      });
     }
   }
 
